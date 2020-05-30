5a19d6a52bd6bbc5f03178a43224c2c17a3af14a
Hitesh Kumar <62385692+Perseus1729@users.noreply.github.com>
2020-05-30 23:13:45 +0530

    Update Python_Code.py

--- Python_Code.py
+++ Python_Code.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 # Tower_of _Hanoi
 def Tower_of_Hanoi(n,start,end,middle):
 	if(n==1):
@@ -8,8 +8,6 @@ def Tower_of_Hanoi(n,start,end,middle):
 	Tower_of_Hanoi(n-1,start,middle,end)
 	print("Move disc from ",start," Rod to",end," Rod")
 	Tower_of_Hanoi(n-1,middle,end,start)
-n=int(sys.argv[1])
-Tower_of_Hanoi(n,'A','C','B')
 
 # Fibonacci
 
@@ -18,14 +16,26 @@ def Fibonacci(n):
 		return n
 	return(Fibonacci(n-1)+Fibonacci(n-2))
 
-x=int(sys.argv[2])
-Fibonacci(x)
 # Factorial
 
 def Factorial(n):
 	if(n==1):
 		return(1)
 	return(n*Factorial(n-1))
+
+n=int(sys.argv[1])
+x=int(sys.argv[2])
 y=int(sys.argv[3])
+
+start_time=time.time()
+Tower_of_Hanoi(n,'A','C','B')
+time_taken=time.time()-start_time
+print("Time_Taken:",time_taken)
+
+print(Fibonacci(x))
+
+start_time=time.time()
 print(Factorial(y))
+time_taken=time.time()-start_time
+print("Time_Taken:",time_taken)
 
