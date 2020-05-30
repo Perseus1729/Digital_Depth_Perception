1d8719aff24b1c4e9db55baf192ca51337e297a9
Hitesh Kumar <62385692+Perseus1729@users.noreply.github.com>
2020-05-30 23:14:44 +0530

    Update py_c.py

--- ddp/py_c.py
+++ ddp/py_c.py
@@ -1,4 +1,19 @@
 import myModule
-print(myModule.fib(10))
-print(myModule.fac(5))
-myModule.TOH(3)
+import time
+import sys
+
+n=int(sys.argv[1])
+x=int(sys.argv[2])
+y=int(sys.argv[3])
+
+print(myModule.fib(x))
+
+start_time=time.time()
+myModule.TOH(n)
+time_taken=time.time()-start_time
+print("Time_Taken:",time_taken)
+
+start_time=time.time()
+print(myModule.fac(y))
+time_taken=time.time()-start_time
+print("Time_Taken:",time_taken)
